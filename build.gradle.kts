9b63d6fc1f14356bf765f00f34bbd11b1a3117fd
Kylee <30977909+ReKylee@users.noreply.github.com>
2026-05-17 02:04:24 +0000

    chore(build): update Android library plugin version to 8.7.0 and Gradle distribution URL to 8.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
 	kotlin("multiplatform") version "2.0.21" apply false
 	kotlin("plugin.serialization") version "2.0.21" apply false
-	id("com.android.library") version "8.5.2" apply false
+	id("com.android.library") version "8.7.0" apply false
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	kotlin("multiplatform") version "2.0.21"
 	kotlin("plugin.serialization") version "2.0.21"
-	id("com.android.library") version "8.5.2"
+	id("com.android.library") version "8.7.0"
 }
 
 kotlin {
